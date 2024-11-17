9a095b8dfe27a0f7dbe0c33c6dcade13b9c11a5d
wwz-sust <wwzsust@163.com>
2024-11-17 15:01:01 +0800

    added closepath.js

--- src/canvas/script/beginpath.js
+++ src/canvas/script/beginpath.js
@@ -1,5 +1,6 @@
 import {$} from "./common";
 
+// 判断是否同一个路径是看是否使用了beginPath(), 而不是看视觉上的收尾相连
 function beginpath(){    
     const canvas = $("canvas");
     const ctx = canvas.getContext("2d");
@@ -7,12 +8,18 @@ function beginpath(){
     ctx.clearRect(0,0,canvas.width, canvas.height);
     
     ctx.lineWidth = 5;
-    ctx.strokeStyle = 'blue';
+    
     ctx.beginPath();
-    ctx.moveTo(100, 200);
-    ctx.lineTo(100, 100);
-    ctx.lineTo(200, 250);
-    ctx.closePath();
+    ctx.strokeStyle = 'blue';
+    ctx.moveTo(100, 100);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+
+    
+    ctx.beginPath();
+    ctx.strokeStyle = 'pink';
+    ctx.moveTo(100,200);
+    ctx.lineTo(200, 200);
     ctx.stroke();
 }
 
--- src/canvas/script/index.js
+++ src/canvas/script/index.js
@@ -1,11 +1,13 @@
 import toDataURL from "./toDataURL.js";
 import draw from "./start.js";
 import beginpath from "./beginpath.js";
-
+import closepath from "./closepath.js";
 // 这里刚开始随便乱画
 draw();
 // 试用toDataURL方法获取图片和一些基本的canvas属性
 // toDataURL();
 
 beginpath();
+
+closepath();
 

58462e2b4ea52ae9052ccff355ed151718421280
wwz-sust <wwzsust@163.com>
2024-11-17 21:50:20 +0800

    add isPonitInPath

--- src/canvas/script/index.js
+++ src/canvas/script/index.js
@@ -2,6 +2,9 @@ import toDataURL from "./toDataURL.js";
 import draw from "./start.js";
 import beginpath from "./beginpath.js";
 import closepath from "./closepath.js";
+import isponitInPath from "./isponitinpath.js";
+
+
 // 这里刚开始随便乱画
 draw();
 // 试用toDataURL方法获取图片和一些基本的canvas属性
@@ -10,4 +13,4 @@ draw();
 beginpath();
 
 closepath();
-
+isponitInPath();
